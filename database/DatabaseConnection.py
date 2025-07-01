a05fca83b8be3ad3a3872151598e9d40e6cef07e
Náthalli <nathallideoliveiramaciel13@gmail.com>
2025-07-01 20:03:31 -0300

    Remove arquivo com credenciais

--- database/DatabaseConnection.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="Meninas.SQL",
-        user="postgres",
-        password="eastw1nd"
-    )
