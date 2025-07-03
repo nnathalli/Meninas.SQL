675a579ae85d47115ae47a655997a65d151c6530
Náthalli <nathallideoliveiramaciel13@gmail.com>
2025-07-03 01:09:32 -0300

    Conexão com o banco

--- database/DatabaseConnection.example.py
+++ database/DatabaseConnection.example.py
@@ -1,9 +1,23 @@
 import psycopg2
 
-def get_connection():
-    return psycopg2.connect(
-        host="SEU_HOST",
-        database="NOME_BANCO",
-        user="SEU_USUARIO",
-        password="SUA_SENHA"
-    )
+class DatabaseConnection:
+    def __init__(self):
+        self.conn = None
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                host="localhost",
+                port=5432,
+                dbname="seu_banco",
+                user="seu_usuario",
+                password="sua_senha"
+            )
+            return self.conn
+        except Exception as e:
+            print("Erro na conexão com o banco:", e)
+            return None
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
